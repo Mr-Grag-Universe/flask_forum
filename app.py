f62c9c9bfac8ac50ad274ef49ac0fc2a7c2adb64
stepan <kafanovstepan@gmail.com>
2022-08-06 22:38:43 +0300

    just save

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+from crypt import methods
 from flask import Flask, render_template
+
 from project.home import home_render
+from project.signin import signin_render
 
 app = Flask(__name__)
 
 app.add_url_rule('/', view_func=home_render, methods=['get', 'post'])
 app.add_url_rule('/index', view_func=home_render, methods=['get', 'post'])
 app.add_url_rule('/home', view_func=home_render, methods=['get', 'post'])
+
+app.add_url_rule('/signin', view_func=signin_render, methods=['get', 'post'])
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5000, debug=True)
---
+++ project/signin.py
@@ -0,0 +1,4 @@
+from flask import Flask, render_template
+
+def signin_render():
+    return render_template('signin.html')
